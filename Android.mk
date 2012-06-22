ff5eae6ffa3c89633607b16167c0b3e919d26ef3
Bruce Beare <bruce.j.beare@intel.com>
2012-06-22 10:54:00 -0700

    build: Clean up use of include files
    BZ: 43321
    
    Projects should not refer to header files in other projects directly
    (since projects can be moved around). Instead... headers should be
    published into the out/ directory and found there when needed.
    
    Change-Id: I252da52d6071b6de6e1fd6207031d2cf5a8eddaa
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Reviewed-on: http://android.intel.com:8080/56278
    Reviewed-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>

--- Android.mk
+++ Android.mk
@@ -3,10 +3,40 @@ ifeq ($(strip $(BOARD_USES_WRS_OMXIL_CORE)),true)
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_COPY_HEADERS_TO := khronos/openmax
 LOCAL_COPY_HEADERS := \
+    core/inc/khronos/openmax/include/OMX_Audio.h \
+    core/inc/khronos/openmax/include/OMX_Component.h \
+    core/inc/khronos/openmax/include/OMX_ContentPipe.h \
+    core/inc/khronos/openmax/include/OMX_Core.h \
+    core/inc/khronos/openmax/include/OMX_IVCommon.h \
+    core/inc/khronos/openmax/include/OMX_Image.h \
+    core/inc/khronos/openmax/include/OMX_Index.h \
     core/inc/khronos/openmax/include/OMX_IndexExt.h \
-    core/inc/khronos/openmax/include/OMX_VideoExt.h \
+    core/inc/khronos/openmax/include/OMX_IntelErrorTypes.h \
+    core/inc/khronos/openmax/include/OMX_Other.h \
+    core/inc/khronos/openmax/include/OMX_Types.h \
+    core/inc/khronos/openmax/include/OMX_Video.h \
+    core/inc/khronos/openmax/include/OMX_VideoExt.h
+include $(BUILD_COPY_HEADERS)
 
+include $(CLEAR_VARS)
+LOCAL_COPY_HEADERS_TO := wrs_omxil_core
+LOCAL_COPY_HEADERS := \
+    base/inc/cmodule.h \
+    base/inc/componentbase.h \
+    base/inc/portaudio.h \
+    base/inc/portbase.h \
+    base/inc/portimage.h \
+    base/inc/portother.h \
+    base/inc/portvideo.h \
+    utils/inc/audio_parser.h \
+    utils/inc/list.h \
+    utils/inc/log.h \
+    utils/inc/module.h \
+    utils/inc/queue.h \
+    utils/inc/thread.h \
+    utils/inc/workqueue.h
 include $(BUILD_COPY_HEADERS)
 
 include $(CLEAR_VARS)
